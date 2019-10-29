721d2da2d51f8b548439a6c1bd15d338320e8920
Hetic <hassa75x13@gmail.com>
2019-10-29 01:45:05 +0100

    first

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Loading
+let loading = document.querySelector('main div.div_logo');
+
+setInterval(remove, 5300);
+
+    function remove(){
+        loading.classList.add("div_logo--hidden");
+    };
+//
+
+let about = document.querySelector('main .first header .about_text');
+let open = document.querySelector('section.about');
+let close = document.querySelector('.close');
+
+about.addEventListener("click", function(){
+    open.classList.toggle("about--open");
+});
+
+close.addEventListener("click", function(){
+    open.classList.remove("about--open");
+});
